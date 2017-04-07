409f824ab9d032e7a0e216d51e94e4e2ee22d776
Monur <monurakkaya@macellan.net>
2017-04-08 01:57:35 +0300

    extend text to LC_ALL

--- tests/TRLocaleTest.php
+++ tests/TRLocaleTest.php
@@ -8,7 +8,7 @@ class TRLocaleTest extends TestCase
      /** @test */
     public function it_can_perform_a_single_item_with_tr_locale()
     {
-        setlocale(LC_TIME, 'tr_TR.UTF-8');
+        setlocale(LC_ALL, 'tr_TR.UTF-8');
         $array = $this->fractal
             ->item($this->testBooks[0], new TestTransformer())
             ->toArray();
