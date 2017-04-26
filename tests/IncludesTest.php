7b3d55d731a35ff5f8459d1dd01af884fc436b80
Freek Van der Herten <freek@spatie.be>
2017-04-26 14:58:08 +0000

    Apply fixes from StyleCI

--- tests/IncludesTest.php
+++ tests/IncludesTest.php
@@ -15,7 +15,7 @@ class IncludesTest extends TestCase
             ->toArray();
 
         $expectedArray = ['data' => [
-            ['id' => 1, 'author' => 'Philip K Dick',  'characters' => ['data' => [['name' => 'Death'],['name' =>  'Hex']]]],
+            ['id' => 1, 'author' => 'Philip K Dick',  'characters' => ['data' => [['name' => 'Death'], ['name' =>  'Hex']]]],
             ['id' => 2, 'author' => 'George R. R. Satan', 'characters' => ['data' => [['name' => 'Ned Stark'], ['name' => 'Tywin Lannister']]]],
         ]];
 
@@ -48,7 +48,7 @@ class IncludesTest extends TestCase
             ->toArray();
 
         $expectedArray = ['data' => [
-            ['id' => 1, 'author' => 'Philip K Dick',  'characters' => ['data' => [['name' => 'Death'],['name' =>  'Hex']]], 'publisher' => ['data' => ['Elephant books']]],
+            ['id' => 1, 'author' => 'Philip K Dick',  'characters' => ['data' => [['name' => 'Death'], ['name' =>  'Hex']]], 'publisher' => ['data' => ['Elephant books']]],
             ['id' => 2, 'author' => 'George R. R. Satan', 'characters' => ['data' => [['name' => 'Ned Stark'], ['name' => 'Tywin Lannister']]], 'publisher' => ['data' => ['Bloody Fantasy inc.']]],
         ]];
 
@@ -65,7 +65,7 @@ class IncludesTest extends TestCase
 
         $expectedArray = ['data' => [
             ['id' => 1, 'author' => 'Philip K Dick',
-                'characters' => ['data' => [['name' => 'Death'],['name' =>  'Hex']]],
+                'characters' => ['data' => [['name' => 'Death'], ['name' =>  'Hex']]],
                 'publisher' => ['data' => ['Elephant books']],
             ],
             ['id' => 2, 'author' => 'George R. R. Satan',
