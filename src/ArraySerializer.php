4d3b8a68b6575e9b0202e842977c9c29224fd9ac
AlexJezior <Alex.Jezior@gmail.com>
2022-03-18 01:02:11 -0700

    fix: Php 8.1 support (#53)
    * Updated league/fractal to 0.20.0. Updated Fractal::jsonSerialize return type to match the JsonSerializable interface. Updated TraversableClass::getIterator() to match the IteratorAggregate interface.
    
    * Updated php minimum supported version from 7.3 to 7.4
    
    * Updated `ArraySerializer::collection` type hints
    
    * Resolved issues with broken tests.

--- src/ArraySerializer.php
+++ src/ArraySerializer.php
@@ -14,7 +14,7 @@ class ArraySerializer extends BaseArraySerializer
      *
      * @return array
      */
-    public function collection($resourceKey, array $data)
+    public function collection(string $resourceKey, array $data): array
     {
         return $data;
     }
