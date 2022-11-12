7994c99bcd862a15c0a407b649de0689528854bd
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-12 18:19:09 +0300

    refactor ScopeTest

--- tests/ScopeTest.php
+++ tests/ScopeTest.php
@@ -1,19 +1,13 @@
 <?php
 
-namespace Spatie\Fractalistic\Test;
-
 use Spatie\Fractalistic\Test\TestClasses\TestTransformer;
+use function PHPUnit\Framework\assertEquals;
 
-class ScopeTest extends TestCase
-{
-    /** @test */
-    public function it_uses_an_identifier_for_the_scope()
-    {
-        $scope = $this->fractal
-            ->collection($this->testBooks, new TestTransformer(), 'books')
-            ->parseIncludes('characters')
-            ->createData();
+it('uses an identifier for the scope', function () {
+    $scope = $this->fractal
+        ->collection($this->testBooks, new TestTransformer(), 'books')
+        ->parseIncludes('characters')
+        ->createData();
 
-        $this->assertEquals('books', $scope->getIdentifier());
-    }
-}
+    assertEquals('books', $scope->getIdentifier());
+});
