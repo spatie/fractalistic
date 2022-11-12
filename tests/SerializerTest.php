b569ac1b13015b8c15f2fdbc5b48797b8755ff55
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-12 18:20:00 +0300

    refactor SerializerTest

--- tests/SerializerTest.php
+++ tests/SerializerTest.php
@@ -1,51 +1,41 @@
 <?php
 
-namespace Spatie\Fractalistic\Test;
-
 use Spatie\Fractalistic\ArraySerializer;
 use Spatie\Fractalistic\Test\TestClasses\TestTransformer;
+use function PHPUnit\Framework\assertEquals;
 
-class SerializerTest extends TestCase
-{
-    /** @test */
-    public function it_does_not_generate_a_data_key_for_a_collection()
-    {
-        $array = $this->fractal
-            ->collection($this->testBooks, new TestTransformer())
-            ->serializeWith(new ArraySerializer())
-            ->toArray();
+it('does not generate a data key for a collection', function () {
+    $array = $this->fractal
+        ->collection($this->testBooks, new TestTransformer())
+        ->serializeWith(new ArraySerializer())
+        ->toArray();
 
-        $expectedArray = [
-            ['id' => 1, 'author' => 'Philip K Dick'],
-            ['id' => 2, 'author' => 'George R. R. Satan'],
-        ];
+    $expectedArray = [
+        ['id' => 1, 'author' => 'Philip K Dick'],
+        ['id' => 2, 'author' => 'George R. R. Satan'],
+    ];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_does_not_generate_a_data_key_for_an_item()
-    {
-        $array = $this->fractal
-            ->item($this->testBooks[0], new TestTransformer())
-            ->serializeWith(new ArraySerializer())
-            ->toArray();
+it('does not generate a data key for an item', function () {
+    $array = $this->fractal
+        ->item($this->testBooks[0], new TestTransformer())
+        ->serializeWith(new ArraySerializer())
+        ->toArray();
 
-        $expectedArray = ['id' => 1, 'author' => 'Philip K Dick'];
+    $expectedArray = ['id' => 1, 'author' => 'Philip K Dick'];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_accepts_a_class_name_for_the_serializer()
-    {
-        $array = $this->fractal
-            ->item($this->testBooks[0], new TestTransformer())
-            ->serializeWith(ArraySerializer::class)
-            ->toArray();
+it('accepts a class name for the serializer', function () {
+    $array = $this->fractal
+        ->item($this->testBooks[0], new TestTransformer())
+        ->serializeWith(ArraySerializer::class)
+        ->toArray();
 
-        $expectedArray = ['id' => 1, 'author' => 'Philip K Dick'];
+    $expectedArray = ['id' => 1, 'author' => 'Philip K Dick'];
 
-        $this->assertEquals($expectedArray, $array);
-    }
-}
+    assertEquals($expectedArray, $array);
+});
