ad8cab933a6f83fa81013bc1986d08b88633e842
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-12 18:22:23 +0300

    remove TestCase

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,9 +6,36 @@
 |--------------------------------------------------------------------------
 */
 
-use Spatie\Fractalistic\Test\TestCase;
+use Spatie\Fractalistic\Fractal;
 
-uses(TestCase::class)->in('.');
+uses()
+    ->beforeEach(function () {
+        $this->defaultSerializer = '';
+
+        $this->fractal = Fractal::create();
+
+        $this->testBooks = [
+            [
+                'id' => '1',
+                'title' => 'Hogfather',
+                'yr' => '1998',
+                'author_name' => 'Philip K Dick',
+                'author_email' => 'philip@example.org',
+                'characters' => [['name' => 'Death'], ['name' => 'Hex']],
+                'publisher' => 'Elephant books',
+            ],
+            [
+                'id' => '2',
+                'title' => 'Game Of Kill Everyone',
+                'yr' => '2014',
+                'author_name' => 'George R. R. Satan',
+                'author_email' => 'george@example.org',
+                'characters' => [['name' => 'Ned Stark'], ['name' => 'Tywin Lannister']],
+                'publisher' => 'Bloody Fantasy inc.',
+            ],
+        ];
+    })
+    ->in('.');
 
 /*
 |--------------------------------------------------------------------------
