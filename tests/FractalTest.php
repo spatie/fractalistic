72a75112c14e4df484d73501d65e9c9a25ff343b
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-12 18:12:59 +0300

    refactor FractalTest

--- tests/FractalTest.php
+++ tests/FractalTest.php
@@ -3,32 +3,29 @@
 namespace Spatie\Fractalistic\Test;
 
 use League\Fractal\Pagination\Cursor;
+use League\Fractal\Resource\ResourceInterface;
 use League\Fractal\Serializer\JsonApiSerializer;
 use Spatie\Fractalistic\ArraySerializer;
 use Spatie\Fractalistic\Fractal;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertInstanceOf;
 
-class FractalTest extends TestCase
-{
-    /** @test */
-    public function it_can_transform_multiple_items_using_a_transformer_to_json()
-    {
-        $json = $this->fractal
-            ->collection($this->testBooks, new TestTransformer())
-            ->toJson();
+it('can transform multiple items using a transformer to json', function () {
+    $json = $this->fractal
+        ->collection($this->testBooks, new TestTransformer())
+        ->toJson();
 
-        $expectedJson = '{"data":[{"id":1,"author":"Philip K Dick"},{"id":2,"author":"George R. R. Satan"}]}';
+    $expectedJson = '{"data":[{"id":1,"author":"Philip K Dick"},{"id":2,"author":"George R. R. Satan"}]}';
 
-        $this->assertEquals($expectedJson, $json);
-    }
+    assertEquals($expectedJson, $json);
+});
 
-    /** @test */
-    public function it_can_accept_a_bitmask_when_converting_to_json()
-    {
-        $json = $this->fractal
-            ->collection($this->testBooks, new TestTransformer())
-            ->toJson(JSON_PRETTY_PRINT);
+it('can accept a bitmask when converting to json', function () {
+    $json = $this->fractal
+        ->collection($this->testBooks, new TestTransformer())
+        ->toJson(JSON_PRETTY_PRINT);
 
-        $expectedJson = <<<'JSON'
+    $expectedJson = <<<'JSON'
 {
     "data": [
         {
@@ -43,226 +40,191 @@ class FractalTest extends TestCase
 }
 JSON;
 
-        $this->assertEquals($expectedJson, $json);
-    }
+    assertEquals($expectedJson, $json);
+});
 
-    /** @test */
-    public function it_can_transform_multiple_items_using_a_transformer_to_an_array()
-    {
-        $array = $this->fractal
-            ->collection($this->testBooks, new TestTransformer())
-            ->toArray();
+it('can transform multiple items using a transformer to an array', function () {
+    $array = $this->fractal
+        ->collection($this->testBooks, new TestTransformer())
+        ->toArray();
 
-        $expectedArray = ['data' => [
-            ['id' => 1, 'author' => 'Philip K Dick'],
-            ['id' => 2, 'author' => 'George R. R. Satan'],
-        ]];
+    $expectedArray = ['data' => [
+        ['id' => 1, 'author' => 'Philip K Dick'],
+        ['id' => 2, 'author' => 'George R. R. Satan'],
+    ]];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_can_transform_a_collection_using_a_callback()
-    {
-        $array = $this->fractal
-            ->collection($this->testBooks, function ($book) {
-                return ['id' => $book['id']];
-            })->toArray();
+it('can transform a collection using a callback', function () {
+    $array = $this->fractal
+        ->collection($this->testBooks, function ($book) {
+        return ['id' => $book['id']];
+        })->toArray();
 
-        $expectedArray = ['data' => [
-            ['id' => 1],
-            ['id' => 2],
-        ]];
+    $expectedArray = ['data' => [
+        ['id' => 1],
+        ['id' => 2],
+    ]];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_can_transform_a_collection_using_a_class_name()
-    {
-        $json = $this->fractal
-            ->collection($this->testBooks, TestTransformer::class)
-            ->toJson();
+it('can transform a collection using a class name', function () {
+    $json = $this->fractal
+        ->collection($this->testBooks, TestTransformer::class)
+        ->toJson();
 
-        $expectedJson = '{"data":[{"id":1,"author":"Philip K Dick"},{"id":2,"author":"George R. R. Satan"}]}';
+    $expectedJson = '{"data":[{"id":1,"author":"Philip K Dick"},{"id":2,"author":"George R. R. Satan"}]}';
 
-        $this->assertEquals($expectedJson, $json);
-    }
+    assertEquals($expectedJson, $json);
+});
 
-    /** @test */
-    public function it_provides_a_method_to_specify_the_transformer()
-    {
-        $array = $this->fractal
-            ->collection($this->testBooks)
-            ->transformWith(new TestTransformer())
-            ->toArray();
+it('provides a method to specify the transformer', function () {
+    $array = $this->fractal
+        ->collection($this->testBooks)
+        ->transformWith(new TestTransformer())
+        ->toArray();
 
-        $expectedArray = ['data' => [
-            ['id' => 1, 'author' => 'Philip K Dick'],
-            ['id' => 2, 'author' => 'George R. R. Satan'],
-        ]];
+    $expectedArray = ['data' => [
+        ['id' => 1, 'author' => 'Philip K Dick'],
+        ['id' => 2, 'author' => 'George R. R. Satan'],
+    ]];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_can_accept_a_class_name_as_a_transformer()
-    {
-        $array = $this->fractal
-            ->collection($this->testBooks)
-            ->transformWith(TestTransformer::class)
-            ->toArray();
+it('can accept a class name as a transformer', function () {
+    $array = $this->fractal
+        ->collection($this->testBooks)
+        ->transformWith(TestTransformer::class)
+        ->toArray();
 
-        $expectedArray = ['data' => [
-            ['id' => 1, 'author' => 'Philip K Dick'],
-            ['id' => 2, 'author' => 'George R. R. Satan'],
-        ]];
+    $expectedArray = ['data' => [
+        ['id' => 1, 'author' => 'Philip K Dick'],
+        ['id' => 2, 'author' => 'George R. R. Satan'],
+    ]];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_can_perform_a_single_item()
-    {
-        $array = $this->fractal
-            ->item($this->testBooks[0], new TestTransformer())
-            ->toArray();
+it('can perform a single item', function () {
+    $array = $this->fractal
+        ->item($this->testBooks[0], new TestTransformer())
+        ->toArray();
 
-        $expectedArray = ['data' => [
-            'id' => 1, 'author' => 'Philip K Dick', ]];
+    $expectedArray = ['data' => [
+        'id' => 1, 'author' => 'Philip K Dick', ]];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_can_create_a_resource()
-    {
-        $resource = $this->fractal
-            ->collection($this->testBooks, new TestTransformer())
-            ->getResource();
+it('can create a resource', function () {
+    $resource = $this->fractal
+        ->collection($this->testBooks, new TestTransformer())
+        ->getResource();
 
-        $this->assertInstanceOf(\League\Fractal\Resource\ResourceInterface::class, $resource);
-    }
+    assertInstanceOf(ResourceInterface::class, $resource);
+});
 
-    /** @test */
-    public function it_can_create_fractal_data()
-    {
-        $resource = $this->fractal
-            ->collection($this->testBooks, new TestTransformer())
-            ->createData();
+it('can create fractal data', function () {
+    $resource = $this->fractal
+        ->collection($this->testBooks, new TestTransformer())
+        ->createData();
 
-        $this->assertInstanceOf(\League\Fractal\Scope::class, $resource);
-    }
+    assertInstanceOf(\League\Fractal\Scope::class, $resource);
+});
 
-    /** @test */
-    public function it_can_perform_a_null_item()
-    {
-        $array = Fractal::create(null, NullableTransformer::class)
-            ->serializeWith(new JsonApiSerializer())
-            ->withResourceName('books')
-            ->toArray();
+it('can perform a null item', function () {
+    $array = Fractal::create(null, NullableTransformer::class)
+        ->serializeWith(new JsonApiSerializer())
+        ->withResourceName('books')
+        ->toArray();
 
-        $expectedArray = ['data' => null];
+    $expectedArray = ['data' => null];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_can_create_a_null_resource()
-    {
-        $resource = Fractal::create(null, NullableTransformer::class)
-            ->serializeWith(new JsonApiSerializer())
-            ->withResourceName('books')
-            ->getResource();
+it('can create a null resource', function () {
+    $resource = Fractal::create(null, NullableTransformer::class)
+        ->serializeWith(new JsonApiSerializer())
+        ->withResourceName('books')
+        ->getResource();
 
-        $this->assertInstanceOf(\League\Fractal\Resource\ResourceInterface::class, $resource);
-    }
+    assertInstanceOf(ResourceInterface::class, $resource);
+});
 
-    /** @test */
-    public function it_can_create_null_fractal_data()
-    {
-        $resource = Fractal::create(null, NullableTransformer::class)
-            ->serializeWith(new JsonApiSerializer())
-            ->withResourceName('books')
-            ->createData();
+it('can create null fractal data', function () {
+    $resource = Fractal::create(null, NullableTransformer::class)
+        ->serializeWith(new JsonApiSerializer())
+        ->withResourceName('books')
+        ->createData();
 
-        $this->assertInstanceOf(\League\Fractal\Scope::class, $resource);
-    }
+    assertInstanceOf(\League\Fractal\Scope::class, $resource);
+});
 
-    /** @test */
-    public function it_provides_chainable_methods()
-    {
-        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->item('test'));
-        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->collection([]));
-        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->primitive(123));
-        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->transformWith(function () {
-        }));
-        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->serializeWith(new ArraySerializer()));
-        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->withCursor(
-            new Cursor(0, null, 10, 10)
-        ));
-        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->addMeta([]));
-        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->paginateWith(
-            $this->createMock('League\Fractal\Pagination\PaginatorInterface')
-        ));
-    }
+it('provides chainable methods', function () {
+    assertInstanceOf(get_class($this->fractal), $this->fractal->item('test'));
+    assertInstanceOf(get_class($this->fractal), $this->fractal->collection([]));
+    assertInstanceOf(get_class($this->fractal), $this->fractal->primitive(123));
+    assertInstanceOf(get_class($this->fractal), $this->fractal->transformWith(function () {
+    }));
+    assertInstanceOf(get_class($this->fractal), $this->fractal->serializeWith(new ArraySerializer()));
+    assertInstanceOf(get_class($this->fractal), $this->fractal->withCursor(
+        new Cursor(0, null, 10, 10)
+    ));
+    assertInstanceOf(get_class($this->fractal), $this->fractal->addMeta([]));
+    assertInstanceOf(get_class($this->fractal), $this->fractal->paginateWith(
+        $this->createMock('League\Fractal\Pagination\PaginatorInterface')
+    ));
+});
 
-    /** @test */
-    public function it_can_perform_an_empty_array()
-    {
-        $array = Fractal::create([], TestTransformer::class)
-            ->toArray();
+it('can perform an empty array', function () {
+    $array = Fractal::create([], TestTransformer::class)
+        ->toArray();
 
-        $expectedArray = ['data' => []];
+    $expectedArray = ['data' => []];
 
-        $this->assertEquals($expectedArray, $array);
-    }
+    assertEquals($expectedArray, $array);
+});
 
-    /** @test */
-    public function it_can_define_collection_after_resource_name()
-    {
-        $resource = Fractal::create()
-            ->withResourceName('tests')
-            ->collection($this->testBooks)
-            ->transformWith(new TestTransformer);
+it('can define collection after resource name', function () {
+    $resource = Fractal::create()
+        ->withResourceName('tests')
+        ->collection($this->testBooks)
+        ->transformWith(new TestTransformer());
 
-        $this->assertEquals('tests', $resource->getResource()->getResourceKey());
-    }
+    assertEquals('tests', $resource->getResource()->getResourceKey());
+});
 
-    /** @test */
-    public function it_can_define_item_after_resource_name()
-    {
-        $resource = Fractal::create()
-            ->withResourceName('tests')
-            ->item($this->testBooks[0])
-            ->transformWith(new TestTransformer);
+it('can define item after resource name', function () {
+    $resource = Fractal::create()
+        ->withResourceName('tests')
+        ->item($this->testBooks[0])
+        ->transformWith(new TestTransformer());
 
-        $this->assertEquals('tests', $resource->getResource()->getResourceKey());
-    }
+    assertEquals('tests', $resource->getResource()->getResourceKey());
+});
 
-    /** @test */
-    public function it_can_define_primitive_after_resource_name()
-    {
-        $resource = Fractal::create()
-            ->withResourceName('tests')
-            ->primitive($this->testBooks[0])
-            ->transformWith(new TestTransformer);
+it('can define primitive after resource name', function () {
+    $resource = Fractal::create()
+        ->withResourceName('tests')
+        ->primitive($this->testBooks[0])
+        ->transformWith(new TestTransformer());
 
-        $this->assertEquals('tests', $resource->getResource()->getResourceKey());
-    }
+    assertEquals('tests', $resource->getResource()->getResourceKey());
+});
 
-    /** @test */
-    public function it_can_convert_into_something_that_is_json_serializable()
-    {
-        $jsonSerialized = $this->fractal
-            ->collection($this->testBooks, new TestTransformer())
-            ->jsonSerialize();
+it('can convert into something that is json serializable', function () {
+    $jsonSerialized = $this->fractal
+        ->collection($this->testBooks, new TestTransformer())
+        ->jsonSerialize();
 
-        $expectedArray = ['data' => [
-            ['id' => 1, 'author' => 'Philip K Dick'],
-            ['id' => 2, 'author' => 'George R. R. Satan'],
-        ]];
+    $expectedArray = ['data' => [
+        ['id' => 1, 'author' => 'Philip K Dick'],
+        ['id' => 2, 'author' => 'George R. R. Satan'],
+    ]];
 
-        $this->assertEquals($expectedArray, $jsonSerialized);
-    }
-}
+    assertEquals($expectedArray, $jsonSerialized);
+});
