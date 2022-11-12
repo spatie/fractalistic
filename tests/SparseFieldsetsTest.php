7b6c6eb1d2f6adba85f57ae2b18c4da0f11afedd
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-12 18:21:02 +0300

    refactor SparseFieldsetsTest

--- tests/SparseFieldsetsTest.php
+++ tests/SparseFieldsetsTest.php
@@ -1,34 +1,28 @@
 <?php
 
-namespace Spatie\Fractalistic\Test;
-
 use Spatie\Fractalistic\ArraySerializer;
 use Spatie\Fractalistic\Fractal;
+use function PHPUnit\Framework\assertEquals;
 
-class SparseFieldsetsTest extends TestCase
-{
-    /** @test */
-    public function it_can_filter_out_fields()
-    {
-        $array = Fractal::create()
-            ->item([
-                'key1' => 'value1',
-                'key2' => 'value2',
-                'key3' => 'value3',
-            ], function ($item) {
-              return $item;
-          }, new ArraySerializer())
-          ->withResourceName('test')
-          ->parseFieldsets(['test' => 'key1,key3'])
-          ->toArray();
+it('can filter out fields', function () {
+    $array = Fractal::create()
+        ->item([
+        'key1' => 'value1',
+        'key2' => 'value2',
+        'key3' => 'value3',
+        ], function ($item) {
+          return $item;
+      }, new ArraySerializer())
+      ->withResourceName('test')
+      ->parseFieldsets(['test' => 'key1,key3'])
+      ->toArray();
 
-        $expectedArray = [
-            'data' => [
-                'key1' => 'value1',
-                'key3' => 'value3',
-            ],
-        ];
+    $expectedArray = [
+        'data' => [
+        'key1' => 'value1',
+        'key3' => 'value3',
+        ],
+    ];
 
-        $this->assertEquals($expectedArray, $array);
-    }
-}
+    assertEquals($expectedArray, $array);
+});
