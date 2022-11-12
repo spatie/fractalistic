c445f8138184a028a748f02e05f53a61de17c687
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-12 18:05:46 +0300

    refactor FractalFunctionHelperTest

--- tests/FractalFunctionHelperTest.php
+++ tests/FractalFunctionHelperTest.php
@@ -1,95 +1,80 @@
 <?php
 
-namespace Spatie\Fractalistic\Test;
-
 use Spatie\Fractalistic\ArraySerializer;
 use Spatie\Fractalistic\Fractal;
+use Spatie\Fractalistic\Test\TestTransformer;
+use Spatie\Fractalistic\Test\TraversableClass;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertInstanceOf;
 
-class FractalFunctionHelperTest extends TestCase
-{
-    /** @test */
-    public function it_returns_an_instance_of_fractal_when_passing_no_arguments()
-    {
-        $this->assertInstanceOf(Fractal::class, Fractal::create());
-    }
+it('returns an instance of fractal when passing no arguments', function () {
+    assertInstanceOf(Fractal::class, Fractal::create());
+});
 
-    /** @test */
-    public function it_can_transform_the_given_array_with_the_given_closure()
-    {
-        $transformedData = Fractal::create(['item1', 'item2'], function ($item) {
-            return ['item' => $item.'-transformed'];
-        })->toArray();
+it('can transform the given array with the given closure', function () {
+    $transformedData = Fractal::create(['item1', 'item2'], function ($item) {
+        return ['item' => $item.'-transformed'];
+    })->toArray();
 
-        $this->assertEquals([
-            'data' => [['item' => 'item1-transformed'], ['item' => 'item2-transformed']],
-        ], $transformedData);
-    }
+    assertEquals([
+        'data' => [['item' => 'item1-transformed'], ['item' => 'item2-transformed']],
+    ], $transformedData);
+});
 
-    /** @test */
-    public function it_can_transform_the_given_item_with_the_given_closure()
-    {
-        $item = new \stdClass();
-        $item->name = 'item1';
+it('can transform the given item with the given closure', function () {
+    $item = new \stdClass();
+    $item->name = 'item1';
 
-        $transformedData = Fractal::create($item, function ($item) {
-            return ["{$item->name}-transformed"];
-        })->toArray();
+    $transformedData = Fractal::create($item, function ($item) {
+        return ["{$item->name}-transformed"];
+    })->toArray();
 
-        $this->assertEquals([
-            'data' => ['item1-transformed'],
-        ], $transformedData);
-    }
+    assertEquals([
+        'data' => ['item1-transformed'],
+    ], $transformedData);
+});
 
-    /** @test */
-    public function it_can_transform_the_given_array_with_the_given_transformer()
-    {
-        $transformedData = Fractal::create($this->testBooks, new TestTransformer())->toArray();
+it('can transform the given array with the given transformer', function () {
+    $transformedData = Fractal::create($this->testBooks, new TestTransformer())->toArray();
 
-        $expectedArray = ['data' => [
-            ['id' => 1, 'author' => 'Philip K Dick'],
-            ['id' => 2, 'author' => 'George R. R. Satan'],
-        ]];
+    $expectedArray = ['data' => [
+        ['id' => 1, 'author' => 'Philip K Dick'],
+        ['id' => 2, 'author' => 'George R. R. Satan'],
+    ]];
 
-        $this->assertEquals($expectedArray, $transformedData);
-    }
+    assertEquals($expectedArray, $transformedData);
+});
 
-    /** @test */
-    public function it_can_transform_the_given_empty_array_with_the_given_transformer()
-    {
-        $transformedData = Fractal::create([], new TestTransformer())->toArray();
+it('can transform the given empty array with the given transformer', function () {
+    $transformedData = Fractal::create([], new TestTransformer())->toArray();
 
-        $expectedArray = ['data' => []];
+    $expectedArray = ['data' => []];
 
-        $this->assertEquals($expectedArray, $transformedData);
-    }
+    assertEquals($expectedArray, $transformedData);
+});
 
-    /** @test */
-    public function it_can_transform_the_given_traversable_with_the_given_transformer()
-    {
-        $transformedData = Fractal::create(new TraversableClass($this->testBooks), new TestTransformer())->toArray();
+it('can transform the given traversable with the given transformer', function () {
+    $transformedData = Fractal::create(new TraversableClass($this->testBooks), new TestTransformer())->toArray();
 
-        $expectedArray = ['data' => [
-            ['id' => 1, 'author' => 'Philip K Dick'],
-            ['id' => 2, 'author' => 'George R. R. Satan'],
-        ]];
+    $expectedArray = ['data' => [
+        ['id' => 1, 'author' => 'Philip K Dick'],
+        ['id' => 2, 'author' => 'George R. R. Satan'],
+    ]];
 
-        $this->assertEquals($expectedArray, $transformedData);
-    }
+    assertEquals($expectedArray, $transformedData);
+});
 
-    /** @test */
-    public function it_perform_a_transformation_with_the_given_serializer()
-    {
-        $transformedData = Fractal::create(
-            $this->testBooks,
-            new TestTransformer(),
-            new ArraySerializer()
-        )->toArray();
+it('perform a transformation with the given serializer', function () {
+    $transformedData = Fractal::create(
+        $this->testBooks,
+        new TestTransformer(),
+        new ArraySerializer()
+    )->toArray();
 
-        $expectedArray = [
-            ['id' => 1, 'author' => 'Philip K Dick'],
-            ['id' => 2, 'author' => 'George R. R. Satan'],
-        ];
+    $expectedArray = [
+        ['id' => 1, 'author' => 'Philip K Dick'],
+        ['id' => 2, 'author' => 'George R. R. Satan'],
+    ];
 
-        $this->assertEquals($expectedArray, $transformedData);
-    }
-}
+    assertEquals($expectedArray, $transformedData);
+});
