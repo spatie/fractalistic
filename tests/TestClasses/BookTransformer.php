b225c40571ab93a064bc00fd94d512aed5ddac28
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-06 10:49:59 +0330

    Create BookTransformer.php

---
+++ tests/TestClasses/BookTransformer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Spatie\Fractalistic\Test\TestClasses;
+
+use League\Fractal\Resource\Item;
+use League\Fractal\TransformerAbstract;
+
+class BookTransformer extends TransformerAbstract
+{
+    protected array $availableIncludes = [
+        'publisher',
+        'author',
+    ];
+
+    public function transform(Book $book): array
+    {
+        return [
+            'id' => (int)$book->id,
+            'title' => $book->title,
+        ];
+    }
+
+    public function includePublisher(Book $book): Item
+    {
+        return $this->item($book->publisher(), new PublisherTransformer());
+    }
+
+    public function includeAuthor(Book $book): Item
+    {
+        return $this->item($book->author(), new AuthorTransformer());
+    }
+}
